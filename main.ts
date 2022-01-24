a65f098d453ed928eec870d469823873fe90c9f1
bradywisdom <98351289+bradywisdom@users.noreply.github.com>
2022-01-24 14:36:47 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,8 +5,42 @@ function addOrRemove (input2: number, up: boolean) {
         return input2 - 1
     }
 }
+let guess = 0
+let mySprite = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+pause(2000)
+let theName = game.askForString("What is your name?", 10)
+mySprite.sayText("Hello " + theName)
+pause(5000)
 addOrRemove(1, Math.percentChance(50))
 game.splash(addOrRemove(1, Math.percentChance(50)))
 game.splash(addOrRemove(2, Math.percentChance(50)))
-game.splash(addOrRemove(15, Math.percentChance(50)))
-game.splash(addOrRemove(30, Math.percentChance(50)))
+pause(5000)
+// picks a random number between 1 and 5
+let value = randint(1, 5)
+// picks a value at random between 1 and 5
+let secoundValue = randint(1, 5)
+// ask to answer the question
+game.splash("Answer the Question")
+while (guess != value + secoundValue) {
+    // telling you the purpose
+    guess = parseFloat(game.askForString("You are guessing the sum of two random numbers between 1 and 5", 12))
+}
+game.splash("correct!")
+game.over(true)
